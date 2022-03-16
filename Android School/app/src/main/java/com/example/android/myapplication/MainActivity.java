355b4e9d579c24dab8b6a460491f5bf510501e0b
LoulwahSaadallah <Lsaadallah1125@gmail.com>
2022-03-16 20:19:11 +0300

    day 5 cw 1 submit
    day 5 cw 1 submission

--- Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ Android School/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -25,7 +25,18 @@ public class MainActivity extends AppCompatActivity {
          *  ضيفوا عناصر من الكلاس
          *  add objects from class Student
          */
+        ArrayList<Student> students = new ArrayList<>();
+       Student s1 = new Student("Yousef", 14, 12, R.drawable. boy1);
+       Student s2 = new Student("Salman" , 13 ,10, R.drawable. boy1 );
+       Student s3 = new Student("Majed", 15, 11, R.drawable. boy1);
 
+       students.add(s1);
+       students.add(s2);
+       students.add(s3);
+       System.out.println("Student name " +students.get(0).getStudentName());
+       System.out.println("Student age" + students.get(0).getStudentAge());
+       System.out.println("Student grade" + students.get(0).getStudentGrade());
+       System.out.println("Student img" + students.get(0).getStudentImg());
 
         StudentAdapter studentAdapter = new StudentAdapter(this,0,students);
 
